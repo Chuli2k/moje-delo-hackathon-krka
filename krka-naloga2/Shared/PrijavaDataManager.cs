ccf479c3985bbaefc4de712c88b8cf604d83b8d8
Marko <marko.culibrk@gmail.com>
2020-04-05 20:10:07 +0200

    Dodal podjetje pri registraciji

---
+++ krka-naloga2/Shared/PrijavaDataManager.cs
@@ -0,0 +1,27 @@
+﻿using krka_naloga2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace krka_naloga2.Shared
+{
+    public interface IPrijavaDataManager
+    {
+        IEnumerable<Podjetje> GetAllPodjetja();
+    }
+
+    public class PrijavaDataManager : IPrijavaDataManager
+    {
+        private readonly IKrkaRepo _krkaRepo;
+        public PrijavaDataManager(IKrkaRepo krkaRepo)
+        {
+            _krkaRepo = krkaRepo;
+        }
+
+        public IEnumerable<Podjetje> GetAllPodjetja()
+        {
+            return _krkaRepo.GetAllPodjetja();
+        }
+    }
+}
