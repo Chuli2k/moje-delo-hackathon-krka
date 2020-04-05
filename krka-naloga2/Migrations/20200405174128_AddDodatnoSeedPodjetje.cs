f616e2f89e5502c19551ea89c432cfdf1f436137
Marko <marko.culibrk@gmail.com>
2020-04-05 19:44:27 +0200

    Narejeno začetno seedanje podatkov.

---
+++ krka-naloga2/Migrations/20200405174128_AddDodatnoSeedPodjetje.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace krka_naloga2.Migrations
+{
+    public partial class AddDodatnoSeedPodjetje : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Podjetja",
+                columns: new[] { "Id", "Naziv", "Sifra" },
+                values: new object[] { 3, "LEK", "10010" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Podjetja",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
